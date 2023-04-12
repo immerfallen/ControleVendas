3f24e3366bab25503dfc4a633855dde655b524ec
immerfallen <marodemelo10@gmail.com>
2023-04-12 13:32:39 -0300

    Criada classe de conexaão com o banco de dados

---
+++ CONEXAO/ConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace ControleVendas.CONEXAO
+{
+    public class ConnectionFactory
+    {
+        public MySqlConnection GetConnection()
+        {
+
+            string conexao = ConfigurationManager.ConnectionStrings["bdvendas"].ConnectionString;
+
+            return new MySqlConnection(conexao);
+        }
+    }
+}
