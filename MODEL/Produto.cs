dbe24e809d30704a59a19f99ef861d281533c959
immerfallen <marodemelo10@gmail.com>
2023-04-17 10:01:45 -0300

    Criada classe produto

---
+++ MODEL/Produto.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleVendas.MODEL
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public decimal Preco { get; set; }
+
+        public int QuantidadeEstoque { get; set; }
+
+        public int FornecedorId { get; set; }
+
+
+    }
+}
