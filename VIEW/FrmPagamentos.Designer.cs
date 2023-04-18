ffd83f5f0618b224ebe274997ef71913b64aaa7c
immerfallen <marodemelo10@gmail.com>
2023-04-18 09:10:20 -0300

    Implementada a tela de vendas

---
+++ VIEW/FrmPagamentos.Designer.cs
@@ -0,0 +1,202 @@
+﻿
+namespace ControleVendas.VIEW
+{
+    partial class FrmPagamentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtDinheiro = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCartao = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTroco = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(122, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(307, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TELA DE PAGAMENTO";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(516, 115);
+            this.panel1.TabIndex = 2;
+            // 
+            // txtDinheiro
+            // 
+            this.txtDinheiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDinheiro.Location = new System.Drawing.Point(245, 192);
+            this.txtDinheiro.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtDinheiro.Name = "txtDinheiro";
+            this.txtDinheiro.Size = new System.Drawing.Size(240, 47);
+            this.txtDinheiro.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label5.Location = new System.Drawing.Point(27, 195);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 39);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Preço (R$):";
+            // 
+            // txtCartao
+            // 
+            this.txtCartao.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCartao.Location = new System.Drawing.Point(245, 265);
+            this.txtCartao.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCartao.Name = "txtCartao";
+            this.txtCartao.Size = new System.Drawing.Size(240, 47);
+            this.txtCartao.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(27, 268);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 39);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Cartão (R$):";
+            // 
+            // txtTroco
+            // 
+            this.txtTroco.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTroco.Location = new System.Drawing.Point(245, 336);
+            this.txtTroco.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTroco.Name = "txtTroco";
+            this.txtTroco.ReadOnly = true;
+            this.txtTroco.Size = new System.Drawing.Size(240, 47);
+            this.txtTroco.TabIndex = 19;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(27, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 39);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Troco:";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotal.Location = new System.Drawing.Point(245, 413);
+            this.txtTotal.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(240, 47);
+            this.txtTotal.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label4.Location = new System.Drawing.Point(27, 416);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 39);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Total:";
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnFinalizar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizar.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnFinalizar.Location = new System.Drawing.Point(34, 486);
+            this.btnFinalizar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(451, 58);
+            this.btnFinalizar.TabIndex = 33;
+            this.btnFinalizar.Text = "Finalizar Venda";
+            this.btnFinalizar.UseVisualStyleBackColor = false;
+            // 
+            // FrmPagamentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 563);
+            this.Controls.Add(this.btnFinalizar);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTroco);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCartao);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDinheiro);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmPagamentos";
+            this.Text = "FrmPagamentos";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtDinheiro;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCartao;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTroco;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnFinalizar;
+        public System.Windows.Forms.TextBox txtTotal;
+    }
+}
