41e6567bb4c317057f1d56210d88f0731a0ba2bc
immerfallen <marodemelo10@gmail.com>
2023-04-18 13:55:57 -0300

    Implementado cadastro de item venda

---
+++ DAO/ItemVendaDAO.cs
@@ -0,0 +1,50 @@
+﻿using ControleVendas.CONEXAO;
+using ControleVendas.MODEL;
+using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace ControleVendas.DAO
+{
+    public class ItemVendaDAO
+    {
+        #region Conexão com o banco de dados
+        private MySqlConnection conexao;
+
+        public ItemVendaDAO()
+        {
+            this.conexao = new ConnectionFactory().GetConnection();
+        }
+        #endregion
+
+        #region CadastrarItemVenda
+
+        public void CadastrarItemVenda(ItemVenda itemVenda)
+        {
+            try
+            {
+                string sql = @"insert into tb_itensvenda (venda_id, produto_id, qtd, subtotal) values(@venda_id, @produto_id, @qtd, @subtotal  )";
+
+                MySqlCommand executaCmd = new MySqlCommand(sql, conexao);
+                executaCmd.Parameters.AddWithValue("@venda_id", itemVenda.VendaId);
+                executaCmd.Parameters.AddWithValue("@produto_id", itemVenda.ProdutoId);
+                executaCmd.Parameters.AddWithValue("@qtd", itemVenda.Quantidade);
+                executaCmd.Parameters.AddWithValue("@subtotal", itemVenda.SubTotal);
+
+                conexao.Open();
+                executaCmd.ExecuteNonQuery();
+
+                MessageBox.Show("Item cadastrado com sucesso");
+
+                conexao.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Ocorreu o erro " + ex.Message);
+            }
+        }
+
+        #endregion
+    }
+}
